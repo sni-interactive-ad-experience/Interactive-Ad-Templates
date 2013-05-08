2c6b560d0e40b458e4b192beb20c2c5712a68e28
Andy <ahutchins@scrippsnetworks.com>
2013-05-08 10:26:20 -0400

    ADded rsi stuff

---
+++ advanced/rsi/js/keywords2013.js
@@ -0,0 +1,112 @@
+// ==============================================
+// = FoodNetwork and Food ASI/RSI Keywords 2013 =
+// ==============================================
+var _keywordList = {
+  hidden_valley_january_2013: [
+    'chicken',
+    'potato',
+    'potatoes',
+    'shrimp',
+    'pasta',
+    'mac',
+    'macaroni'],
+  wishbone_january_2013: [
+    'chicken',
+    'buffalo',
+    'wings',
+    'appetizer',
+    'appetizers'],
+  national_pork_board_2013: [
+    'pork',
+    'tenderloin'],
+  target_february_2013: [
+    'appetizer',
+    'appetizers',
+    'chicken',
+    'easy',
+    'dessert',
+    'desserts',
+    'easter'],
+  johnsonville_february_2013: [
+    'chili',
+    'lasagna',
+    'rigatoni',
+    'frittata',
+    'risotto',
+    'soup',
+    'skewers',
+    'meatloaf',
+    'meatball',
+    'meatballs',
+    'pizza',
+    'stuffing',
+    'stuffed mushrooms'],
+  alaska_seafood_february_2013: [
+    'healthy',
+    'easy',
+    'seafood',
+    'whitefish',
+    'quick'],
+  fisher_nuts_february_2013: [
+    'appetizer',
+    'appetizers',
+    'dessert',
+    'desserts',
+    'salad',
+    'salads',
+    'side dishes'
+  ],
+  best_foods_february_2013: [
+    'chicken',
+    'creamy',
+    'fry'
+  ],
+  hellmans_february_2013: [
+    'chicken',
+    'creamy',
+    'fry'
+  ],
+  vitamix_march_2013: [
+    'fruit',
+    'smoothie',
+    'smoothies',
+    'vinaigrette',
+    'dressing',
+    'soup',
+    'soups',
+    'snack',
+    'snacks',
+    'drink',
+    'drinks',
+    'breakfast',
+    'dip',
+    'dips',
+    'beverage',
+    'beverages',
+    'bread',
+    'dessert',
+    'desserts'
+  ],
+  national_pork_pork_april_2013: [
+    'pork'
+  ],
+  ortega_april_2013: [
+    'mexican',
+    'quick ',
+    'easy',
+    'taco',
+    'tacos',
+    'weeknight',
+    'weeknights',
+    'beef'
+  ],
+  carnation_april_2013: [
+    'breakfast',
+    'easy',
+    'vanilla',
+    'healthy',
+    'smoothie',
+    'smoothies'
+  ]
+};
+$siStartup();
